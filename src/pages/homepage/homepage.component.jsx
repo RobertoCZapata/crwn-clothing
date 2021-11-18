73c133d92a567d8f12bc6f22eeba8ee2bb506154
RobertoCZapata <robertozapataz25@gmail.com>
2021-11-18 17:37:19 -0500

    broken our homepage into smaller components, finish styling menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import Directory from "../../components/directory/directory.component";
+
+import "./homepages.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
